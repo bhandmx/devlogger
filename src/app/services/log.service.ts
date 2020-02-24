f900b4e21dacccc6ebd84ee0e25eed08bd4832ae
Bhandari <minakshi.bhandari@abbott.com>
2020-02-24 13:23:13 +0530

    adds navigation page

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { Log } from "../models/Log";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/observable";
+import { of } from "rxjs/observable/of";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({id: null,text: null,date: null});
+
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean> (true);
+
+  stateClear = this.stateSource.asObservable();
+
+  constructor() { 
+    // this.logs= [
+    //   {id: '1', text: 'Generated Components', date: new Date('12/11/1888 12:45:54') },
+    //   {id: '2', text: 'Added Bootstrap', date: new Date('11/11/1880 12:45:54') }
+    
+    // ]
+    this.logs = [];
+  }
+
+  getLogs():Observable<Log[]> {
+    if(localStorage.getItem('logs') === null){
+      this.logs= [];
+    }
+    else{
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+    return of(this.logs.sort((a , b) => {
+      return b.date = a.date;
+    }));
+
+  }
+
+  setFormLog(log: Log){
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log){
+    this.logs.unshift(log);
+    // Add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+  
+  updateLog(log: Log){
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+
+      }
+    });
+    this.logs.unshift(log);
+     // Update local storage
+     localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  deleteLog(log: Log){
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+
+      }
+    });
+  
+     // Delete from local storage
+     localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  clearState(){
+    this.stateSource.next(true);
+  }
+}
